92d629b5d2a8ea2faa8900416dee8ce89ae9b914
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2023-08-30 20:14:01 +0530

    changes

--- JS/Book/script.js
+++ JS/Book/script.js
@@ -40,3 +40,44 @@ c = new Book(`lol3`,`boi`, 123, false)
 addBookToLibrary(a)
 addBookToLibrary(b)
 addBookToLibrary(c)
+
+
+// Data Structures
+
+class Book {
+  constructor(
+    title = 'Unknown',
+    author = 'Unknown',
+    pages = '0',
+    isRead = false
+  ) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.isRead = isRead
+  }
+}
+
+class Library {
+  constructor() {
+    this.books = []
+  }
+
+  addBook(newBook) {
+    if (!this.isInLibrary(newBook)) {
+      this.books.push(newBook)
+    }
+  }
+
+  removeBook(title) {
+    this.books = this.books.filter((book) => book.title !== title)
+  }
+
+  getBook(title) {
+    return this.books.find((book) => book.title === title)
+  }
+
+  isInLibrary(newBook) {
+    return this.books.some((book) => book.title === newBook.title)
+  }
+}
---
+++ JS/tic-tac-toe/script.js
@@ -0,0 +1,12 @@
+const board = document.querySelector(`.board`);
+
+var gridIndex = 0
+while (gridIndex < 9){
+    const grid = document.createElement(`div`);
+    grid.classList.add(`grid`);
+    grid.classList.add(`grid-${gridIndex +1 }`);
+    board.appendChild(grid)
+    gridIndex ++;
+}
+
+console.log(board)
