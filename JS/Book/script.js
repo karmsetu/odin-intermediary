c3950bfd380d1a5ce47424fd9bf1ca0cbd81b69a
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2023-08-31 03:02:02 +0530

    doing as told

--- JS/Book/script.js
+++ JS/Book/script.js
@@ -1,83 +1,94 @@
-const displayDiv = document.querySelector(`.show-case`)
-console.log(displayDiv)
+const display = document.querySelector(`.show-case`)
+
+
+const formauthor = document.querySelector(`.author`) 
+const formtitle = document.querySelector(`.title`) 
+const formpages = document.querySelector(`.pages`) 
+const formwetherread = document.querySelectorAll(`.wether-read`) 
 const myLibrary = [];
 
-class Book{
-  constructor(title, author, pages, read=false){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-  }
+function Book(title, author, pages, isRead) {
+  // the constructor...
+  this.title = title
+  this.author = author
+  this.pages= pages
+  this.isRead = isRead  
 }
 
-// function Book() {
-//   // the constructor...
-//     this.title = title;
-//     this.author = author;
-//     this.pages = pages;
-//     this.read = read;
-// }
-
-function addBookToLibrary(obj) {
+function addBookToLibrary(book) {
   // do stuff here
-  myLibrary.push(obj)
+  myLibrary.push(book)
+}
 
+function clear(params) {
+  var child = params.lastElementChild; 
+  while (child) {
+    params.removeChild(child);
+    child = params.lastElementChild;
+  }
 }
 
 function showcase() {
-  let whileIndex = 0;
-  displayDivArray = []
-  while (myLibrary.length > whileIndex) {
-    whileIndex ++ 
+  clear(display)
+  for (let index = 0; index < myLibrary.length; index++) {
+    let bookCard = document.createElement(`div`)
+    bookCard.classList.add(`book`)
+    bookCard.classList.add(`book-${index+1}`)
+    let displaytitle = myLibrary[index].title
+    const displaytitleDiv = document.createElement("div")
+    displaytitleDiv.innerText = displaytitle
+    bookCard.append(displaytitleDiv)
+    let displayauthor = myLibrary[index].author
+    const displayauthorDiv = document.createElement("div")
+    displayauthorDiv.innerText = displayauthor
+    bookCard.append(displayauthorDiv)
+    let displaypages = myLibrary[index].pages
+    const displaypagesDiv = document.createElement("div")
+    displaypagesDiv.innerText = displaypages
+    bookCard.append(displaypagesDiv)
+    let displayisRead = myLibrary[index].isRead
+    const displayisReadDiv = document.createElement("div")
+    displayisReadDiv.innerText = displayisRead
+    bookCard.append(displayisReadDiv)
+    display.appendChild(bookCard)
+    console.log(typeof(displayauthorDiv))
   }
 }
 
-a = new Book(`lol1`,`boi`, 123, false)
-b = new Book(`lol2`,`boi`, 123, true)
-c = new Book(`lol3`,`boi`, 123, false)
-
-addBookToLibrary(a)
-addBookToLibrary(b)
-addBookToLibrary(c)
 
 
-// Data Structures
+// pa = new Book(`lol`, `boi`, 4000, true)
+// LOTR = new Book(`lord of the rings`, `boi`, 3000, true)
+// addBookToLibrary(LOTR)
+// addBookToLibrary(pa)
+// showcase()
 
-class Book {
-  constructor(
-    title = 'Unknown',
-    author = 'Unknown',
-    pages = '0',
-    isRead = false
-  ) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.isRead = isRead
-  }
+function openDialog() {
+  document.querySelector(`.dialog`).classList.remove(`close`)
 }
 
-class Library {
-  constructor() {
-    this.books = []
-  }
+const submitButton = document.getElementById(`submit`)
+console.log(submit)
+const author = document.getElementById(`author`)
+console.log(author)
+const title = document.getElementById(`title`)
+console.log(title)
+const pages = document.getElementById(`pages`)
+console.log(pages)
+const isreadtrue = document.getElementById(`is-read-true`)
+const isreadfalse = document.getElementById(`is-read-false`)
 
-  addBook(newBook) {
-    if (!this.isInLibrary(newBook)) {
-      this.books.push(newBook)
-    }
-  }
-
-  removeBook(title) {
-    this.books = this.books.filter((book) => book.title !== title)
-  }
-
-  getBook(title) {
-    return this.books.find((book) => book.title === title)
-  }
-
-  isInLibrary(newBook) {
-    return this.books.some((book) => book.title === newBook.title)
-  }
+boi = []
+function getData() {
+  console.log(author.value)
+  boi.push(author)
+  console.log(title.value)
+  boi.push(title)
+  console.log(pages.value)
+  boi.push(pages)
+  // console.log(isreadtrue.value)
+  // boi.push(isreadtrue)
+  // console.log(isreadfalse.value)
+  // boi.push(isreadfalse)
 }
+console.log(boi)
