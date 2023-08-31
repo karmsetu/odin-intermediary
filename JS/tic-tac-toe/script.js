0297b6619ef28d71ad6c822f609c0d0f26c83168
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2023-08-31 16:25:45 +0530

    A glorious disaster: check function has been completed

--- JS/tic-tac-toe/script.js
+++ JS/tic-tac-toe/script.js
@@ -21,8 +21,32 @@ const gameBoard =(() => {
         }
         // console.log(cell)
     }
+
+    const check = (params)=> {
+        console.log(params)
+        //horizontal
+         //horizontal
+        if((params[0]==`X`&&params[1]==`X`&&params[2]==`X`) || (params[3]==`X`&&params[4]==`X`&&params[5]==`X`) || (params[6]==`X`&&params[7]==`X`&&params[8]==`X`)){
+            console.log(`horizontal`)
+        } else if ((params[0]==`X`&&params[3]==`X`&&params[6]==`X`) || (params[1]==`X`&&params[4]==`X`&&params[7]==`X`) || (params[2]==`X`&&params[5]==`X`&&params[8]==`X`)) { //vertical logic
+            console.log(`vertical x`)
+        } else if ((params[0]==`X` && params[4]==`X` && params[8]==`X`) || (params[2]==`X` && params[4]==`X` && params[6]==`X`)) {
+            console.log(`diagonal x`)
+        }
+
+
+        //horizontal O
+        if((params[0]==`O`&&params[1]==`O`&&params[2]==`O`) || (params[3]==`O`&&params[4]==`O`&&params[5]==`O`) || (params[6]==`O`&&params[7]==`O`&&params[8]==`O`)){
+            console.log(`horizontal O`)
+        } else if ((params[0]==`O`&&params[3]==`O`&&params[6]==`O`) || (params[1]==`O`&&params[4]==`O`&&params[7]==`O`) || (params[2]==`O`&&params[5]==`O`&&params[8]==`O`)) { //vertical logic
+            console.log(`vertical O`)
+        } else if ((params[0]==`O` && params[4]==`O` && params[8]==`O`) || (params[2]==`O` && params[4]==`O` && params[6]==`O`)) {
+            console.log(`diagonal O`)
+        }
+    }
+
     const start =()=>{
-        const x = Player(0, `X`)
+        const x = Player()
         let typeX = true
         let type = `X`
         let cell = document.querySelectorAll(`.grid`)
@@ -35,10 +59,15 @@ const gameBoard =(() => {
                     typeX = true
                     type = `X`
                 }
+                if (params.innerText == `X` || params.innerText == `O`) {
+                    alert(`already taken`)
+                    return
+                }
                 // console.log(params)
                 console.log(params.dataset.grid )
                 console.log(`clicked!`)
                 x.putAndReRender(params.dataset.grid, type)
+                check(x.gameBoardArray);
             })
             // return params
         })
