823a84926236900b5c7856cceffd71dad7f0e126
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2023-09-02 22:19:25 +0530

    OOP in action

---
+++ JS/OOP/script.js
@@ -0,0 +1,8 @@
+// https://duncan-mcardle.medium.com/solid-principle-1-single-responsibility-javascript-5d9ce2c6f4a5
+
+// Single Responsibility Principle
+// states that a class (or object or module… you get the point) should only have one responsibility. This doesn’t mean that an object can only do one thing, but it does mean that everything an object does should be part of one responsibility.
+
+// The open-closed principle says that code should be open for extension, but closed for modification
+
+// The Liskov substitution principle states that any class should be substitutable for its parent class without unexpected consequences
