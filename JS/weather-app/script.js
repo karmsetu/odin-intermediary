ce877a6315c16322d0ed7ed98a801e09e50b472d
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2023-09-05 03:18:42 +0530

    basic ui (needs css)

--- JS/weather-app/script.js
+++ JS/weather-app/script.js
@@ -1,9 +1,12 @@
 const searchBar = document.getElementById(`search-bar`)
+const searchBtn = document.getElementById(`search-button`)
 const contentDisplay = document.querySelector(`.content`)
+const dataPoint = document.querySelectorAll(`.data-point`)
 const API_KEY = `96a2b63f7c7448fb87d101708230309`
-let url = ` http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=paris`
 
 async function call() {
+    let searchQuery = searchBar.value
+    let url = ` http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${searchQuery}`
     const fetched = await fetch(url) 
     const fetchedData = await fetched.json()
     // for (let index = 0; index < fetchedData.current; index++) {
@@ -11,8 +14,15 @@ async function call() {
         
     // }
     displayData = fetchedData.current
-    console.log({displayData})
+    dataPoint[0].innerHTML = displayData.condition.text
+    dataPoint[1].innerHTML = displayData.feelslike_c
+    dataPoint[2].innerHTML = displayData.gust_kph
+    dataPoint[3].innerHTML = displayData.humidity
+    // contentDisplay.innerHTML = 
+    console.log({fetchedData, searchQuery})
 }
 
-call()
+console.log({searchBtn})
+searchBtn.addEventListener(`click`, call)
+// call()
 
