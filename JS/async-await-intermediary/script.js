7d78c864c2834f299aa1b36f5ab1fc10204ec8f5
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2023-09-04 22:47:59 +0530

    Async Await practice

---
+++ JS/async-await-intermediary/script.js
@@ -0,0 +1,36 @@
+console.log(`yo`)
+const img = document.querySelector(`.pic`)
+const searchBar = document.querySelector(`.search-bar`)
+const searchBtn = document.querySelector(`.search`)
+
+// function call(params, index = 3) {
+    
+//     API_KEY = `jhKRwHw5tkdNvQrIMdqoQBadySgaPB3I`
+//     let search = `cats`
+//     fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&limit=20&q=${params}`)
+//         .then(Response => Response.json())
+//         .then(content => {
+//             // console.log(content.data)
+//             img.src = content.data[index].images.downsized.url
+//         })
+//         .catch(error => {
+//             console.error(error);
+//         })
+// }
+
+searchBtn.addEventListener(`click`, e =>{
+    // console.log(searchBar.value)
+    apiCalls(searchBar.value)
+} )
+
+async function apiCalls(params, index=1) {
+    API_KEY = `jhKRwHw5tkdNvQrIMdqoQBadySgaPB3I`
+    const fetched = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&limit=20&q=${params}`);
+    const fetchedData = await fetched.json()
+    console.log(fetchedData)
+    img.src = fetchedData.data[0].images.downsized.url
+
+//     catch (error){
+//         console.error(error);
+//     }
+}
