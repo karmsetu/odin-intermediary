6b4b8fce759b80019710b15cf1535a20f4952f57
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2023-09-07 14:55:03 +0530

    space&time complexity

---
+++ JS/time-complexity/script.js
@@ -0,0 +1,9 @@
+// The way to measure code efficiency is to evaluate how many ‘steps’ it takes to complete. If you know that one algorithm you write takes 5 steps and another one takes 20 steps to accomplish the same task, then you can say that the 5-step algorithm will always run faster than the 20-step algorithm on the same computer.
+
+
+// Asymptotic notations
+// Simply put, Asymptotic Notations are used to describe the running time of an algorithm. Because an algorithm’s running time can differ depending on the input, there are several notations that measure that running time in different ways. The 3 most common are as follows:
+
+// Big O Notation - represents the upper bound of an algorithm. This means the worst-case scenario for how the algorithm will perform.
+// Omega Notation - represents the lower bound of an algorithm. This is the best-case scenario.
+// Theta Notation - represents both the upper bound and lower bound and therefore analyses the average case complexity of an algorithm.
